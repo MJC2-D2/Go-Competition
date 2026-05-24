40e34caa4714c5e21038fd04a6c601db9ae283c3
petecrocker <peter.crocker@oxfordsemantic.tech>
2026-05-24 22:25:22 +0100

    Update to random If the opponent has no legal moves, we can pass

--- players/random_player.py
+++ players/random_player.py
@@ -6,10 +6,17 @@ def name():
 
 
 def version():
-    return "1.0"
+    return "1.1"
 
 
 def select_move(color, board, preview_move, get_legal_moves):
+    opponent = 1 if color == 2 else 2
+
+    # If the opponent has no legal moves, we can pass and win immediately.
+    if not get_legal_moves(opponent, board):
+        return None
+
+    # Otherwise, select a random legal move.
     legal_moves = get_legal_moves(color, board)
     if not legal_moves:
         return None
